1961e87e4a5b74f262ba769e59d5fd1fc813e660
Zoe Dan Zhang <dzha937@aucklanduni.ac.nz>
2025-04-23 17:17:45 +1200

    create Focal and Dice loss based on nnUnetv2

--- nnunetv2/training/nnUNetTrainer/variants/loss/nnUNetTrainerFocalLoss.py
+++ nnunetv2/training/nnUNetTrainer/variants/loss/nnUNetTrainerFocalLoss.py
@@ -1,11 +1,32 @@
 import torch
 from nnunetv2.training.loss.deep_supervision import DeepSupervisionWrapper
 from nnunetv2.training.nnUNetTrainer.nnUNetTrainer import nnUNetTrainer
-from nnunetv2.training.loss.robust_ce_loss import RobustCrossEntropyLoss
+from nnunetv2.training.loss.compound_losses import DC_and_CE_loss
+from nnunetv2.training.loss.dice import MemoryEfficientSoftDiceLoss
 import numpy as np
 
 from nnunetv2.training.nnUNetTrainer.variants.loss.nnUNetTrainerV2_Loss_FL_and_CE import FL_and_CE_loss
 
+class nnUNetTrainerFocalDice(nnUNetTrainer):
+    def _build_loss(self):
+        assert not self.label_manager.has_regions, "regions not supported by this trainer"
+        dice_loss = DC_and_CE_loss({'batch_dice': self.configuration_manager.batch_dice,
+                               'smooth': 1e-5, 'do_bg': False, 'ddp': self.is_ddp}, {}, weight_ce=1, weight_dice=1,
+                              ignore_label=self.label_manager.ignore_label, dice_class=MemoryEfficientSoftDiceLoss)
+        loss = FL_and_CE_loss(alpha=1) + dice_loss
+
+        # we give each output a weight which decreases exponentially (division by 2) as the resolution decreases
+        # this gives higher resolution outputs more weight in the loss
+        if self.enable_deep_supervision:
+            deep_supervision_scales = self._get_deep_supervision_scales()
+            weights = np.array([1 / (2**i) for i in range(len(deep_supervision_scales))])
+            weights[-1] = 0
+
+            # we don't use the lowest 2 outputs. Normalize weights so that they sum to 1
+            weights = weights / weights.sum()
+            # now wrap the loss
+            loss = DeepSupervisionWrapper(loss, weights)
+        return loss
 
 class nnUNetTrainerFocalLoss(nnUNetTrainer):
     def _build_loss(self):
