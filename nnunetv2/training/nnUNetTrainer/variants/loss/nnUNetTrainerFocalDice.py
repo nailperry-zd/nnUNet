c74ff39791d000e6d42e92ef51472c16a0b6d6b4
Zoe Dan Zhang <dzha937@aucklanduni.ac.nz>
2025-04-23 17:23:07 +1200

    update Focal and Dice loss based on nnUnetv2

--- nnunetv2/training/nnUNetTrainer/variants/loss/nnUNetTrainerFocalDice.py
+++ nnunetv2/training/nnUNetTrainer/variants/loss/nnUNetTrainerFocalDice.py
@@ -1,14 +1,19 @@
 import torch
 from nnunetv2.training.loss.deep_supervision import DeepSupervisionWrapper
 from nnunetv2.training.nnUNetTrainer.nnUNetTrainer import nnUNetTrainer
+from nnunetv2.training.loss.compound_losses import DC_and_CE_loss
+from nnunetv2.training.loss.dice import MemoryEfficientSoftDiceLoss
 import numpy as np
 
 from nnunetv2.training.nnUNetTrainer.variants.loss.nnUNetTrainerV2_Loss_FL_and_CE import FL_and_CE_loss
 
-class nnUNetTrainerFocalLoss(nnUNetTrainer):
+class nnUNetTrainerFocalDice(nnUNetTrainer):
     def _build_loss(self):
         assert not self.label_manager.has_regions, "regions not supported by this trainer"
-        loss = FL_and_CE_loss(alpha=0.5)
+        dice_loss = DC_and_CE_loss({'batch_dice': self.configuration_manager.batch_dice,
+                               'smooth': 1e-5, 'do_bg': False, 'ddp': self.is_ddp}, {}, weight_ce=1, weight_dice=1,
+                              ignore_label=self.label_manager.ignore_label, dice_class=MemoryEfficientSoftDiceLoss)
+        loss = FL_and_CE_loss(alpha=1) + dice_loss
 
         # we give each output a weight which decreases exponentially (division by 2) as the resolution decreases
         # this gives higher resolution outputs more weight in the loss
@@ -22,17 +27,3 @@ class nnUNetTrainerFocalLoss(nnUNetTrainer):
             # now wrap the loss
             loss = DeepSupervisionWrapper(loss, weights)
         return loss
-
-
-class nnUNetTrainerFocalLoss_5epochs(nnUNetTrainerFocalLoss):
-    def __init__(
-        self,
-        plans: dict,
-        configuration: str,
-        fold: int,
-        dataset_json: dict,
-        device: torch.device = torch.device("cuda"),
-    ):
-        """used for debugging plans etc"""
-        super().__init__(plans, configuration, fold, dataset_json, device)
-        self.num_epochs = 5
