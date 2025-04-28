1c8136758f15273501c2c2b2728ba3698c29432b
Zoe Dan Zhang <dzha937@aucklanduni.ac.nz>
2025-04-29 11:37:32 +1200

    loss:nnUNetTrainerFocal05Dice03 and nnUNetTrainerFocal05Dice01

--- nnunetv2/training/nnUNetTrainer/variants/loss/nnUNetTrainerFocalDice.py
+++ nnunetv2/training/nnUNetTrainer/variants/loss/nnUNetTrainerFocalDice.py
@@ -245,3 +245,51 @@ class nnUNetTrainerFocal05Dice05(nnUNetTrainer):
             # now wrap the loss
             loss = DeepSupervisionWrapper(loss, weights)
         return loss
+
+class nnUNetTrainerFocal05Dice03(nnUNetTrainer):
+    def _build_loss(self):
+        assert not self.label_manager.has_regions, "regions not supported by this trainer"
+        loss = Focal_DC_and_CE_loss({'batch_dice': self.configuration_manager.batch_dice,
+                                     'smooth': 1e-5, 'do_bg': False, 'ddp': self.is_ddp}, {},
+                                    weight_ce=0.5,
+                                    weight_dice=0.3,
+                                    weight_fl=0.5,
+                                    ignore_label=self.label_manager.ignore_label,
+                                    dice_class=MemoryEfficientSoftDiceLoss)
+
+        # we give each output a weight which decreases exponentially (division by 2) as the resolution decreases
+        # this gives higher resolution outputs more weight in the loss
+        if self.enable_deep_supervision:
+            deep_supervision_scales = self._get_deep_supervision_scales()
+            weights = np.array([1 / (2 ** i) for i in range(len(deep_supervision_scales))])
+            weights[-1] = 0
+
+            # we don't use the lowest 2 outputs. Normalize weights so that they sum to 1
+            weights = weights / weights.sum()
+            # now wrap the loss
+            loss = DeepSupervisionWrapper(loss, weights)
+        return loss
+
+class nnUNetTrainerFocal05Dice01(nnUNetTrainer):
+    def _build_loss(self):
+        assert not self.label_manager.has_regions, "regions not supported by this trainer"
+        loss = Focal_DC_and_CE_loss({'batch_dice': self.configuration_manager.batch_dice,
+                                     'smooth': 1e-5, 'do_bg': False, 'ddp': self.is_ddp}, {},
+                                    weight_ce=0.5,
+                                    weight_dice=0.1,
+                                    weight_fl=0.5,
+                                    ignore_label=self.label_manager.ignore_label,
+                                    dice_class=MemoryEfficientSoftDiceLoss)
+
+        # we give each output a weight which decreases exponentially (division by 2) as the resolution decreases
+        # this gives higher resolution outputs more weight in the loss
+        if self.enable_deep_supervision:
+            deep_supervision_scales = self._get_deep_supervision_scales()
+            weights = np.array([1 / (2 ** i) for i in range(len(deep_supervision_scales))])
+            weights[-1] = 0
+
+            # we don't use the lowest 2 outputs. Normalize weights so that they sum to 1
+            weights = weights / weights.sum()
+            # now wrap the loss
+            loss = DeepSupervisionWrapper(loss, weights)
+        return loss
