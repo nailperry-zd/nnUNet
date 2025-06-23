fe0e5e0150e15bcaeb54054bfc29589454898de9
Zoe Dan Zhang <dzha937@aucklanduni.ac.nz>
2025-06-23 19:06:08 +1200

    remove wrong comment

--- nnunet/training/network_training/spl.py
+++ nnunet/training/network_training/spl.py
@@ -25,7 +25,6 @@ class SymmetricSelfPacedLearning(nn.Module):
         weight_matrix = torch.ones(len(keys))
         for i, key in enumerate(keys):
             weight_matrix[i] = self.weight_map.get(key, 1)
-            print(f"key={key} is not found")
         print(f"weight_matrix={weight_matrix}, device={weight_matrix.device}")
         weight_matrix = weight_matrix.to(loss.device).detach()
         loss = loss * weight_matrix
