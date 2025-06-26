8cac4d1d86f6fae1fbbe528d728bfebada68a0d8
Zoe Dan Zhang <dzha937@aucklanduni.ac.nz>
2025-06-26 21:20:45 +1200

    dicescore_map scalar value

--- nnunet/training/network_training/nnUNetTrainerV2.py
+++ nnunet/training/network_training/nnUNetTrainerV2.py
@@ -337,7 +337,7 @@ class nnUNetTrainerV2(nnUNetTrainer):
                         norm_channels.append(norm_per_channel)
                     self.gradients_map[keys[i]] = norm
                     self.gradients_map_channels[keys[i]] = norm_channels
-                    self.dicescore_map[keys[i]] = dice_index[i]
+                    self.dicescore_map[keys[i]] = dice_index[i].item()
                     # self.focalloss_map[keys[i]] = result_fl[i]
                 del data
         else:
