a021611ad1b1a37a6702f49a207a76bf9e530279
Zoe Dan Zhang <dzha937@aucklanduni.ac.nz>
2025-07-12 15:10:43 +1200

    Hard Always

--- nnunet/training/network_training/nnUNetTrainerV2.py
+++ nnunet/training/network_training/nnUNetTrainerV2.py
@@ -327,8 +327,8 @@ class nnUNetTrainerV2(nnUNetTrainer):
                 #         sitk.WriteImage(sitk_image, filename)
                 #
                 # print("Groundtruth saving completed!")
-                result_fl = self.fl(output[0], target[0])
-                dice_index = self.dice(output[0], target[0])
+                # result_fl = self.fl(output[0], target[0])
+                # dice_index = self.dice(output[0], target[0])
                 for i in range(data_grad.shape[0]):  # Iterate over each batch
                     norm = np.linalg.norm(data_grad[i])  # Calculate L2 norm
                     norm_channels = []
@@ -337,8 +337,8 @@ class nnUNetTrainerV2(nnUNetTrainer):
                         norm_channels.append(norm_per_channel)
                     self.gradients_map[keys[i]] = norm
                     self.gradients_map_channels[keys[i]] = norm_channels
-                    self.dicescore_map[keys[i]] = dice_index[i].item()
-                    self.focalloss_map[keys[i]] = result_fl[i].item()
+                    # self.dicescore_map[keys[i]] = dice_index[i].item()
+                    # self.focalloss_map[keys[i]] = result_fl[i].item()
                 del data
         else:
             output = self.network(data)
--- nnunet/training/network_training/spl.py
+++ nnunet/training/network_training/spl.py
@@ -15,7 +15,7 @@ class SymmetricSelfPacedLearning(nn.Module):
         self.eta = 1
         self.current_epoch = current_epoch + 1
         # self.epoch_step_size = 2 / (1000 - 1)
-        self.epoch_step_size = 2 / 1000
+        self.epoch_step_size = 1 / 1000
         self.weight_first = 2 - self.current_epoch * self.epoch_step_size
         self.weight_last = 2 - self.weight_first
         print(f"weight_first = {self.weight_first}, weight_last={self.weight_last}")
@@ -93,8 +93,8 @@ class FocalLossNonBatch_SPL(nn.Module):
     def forward(self, logit, target, current_epoch, do_backprop, keys, gradients_map):
         result_fl = self.fl(logit, target)
         print(f"FocalLoss is {result_fl}, device={result_fl.device}")
-        dice_index = self.dice(logit, target)
-        print(f"dc score is {dice_index}, device={dice_index.device}")
+        # dice_index = self.dice(logit, target)
+        # print(f"dc score is {dice_index}, device={dice_index.device}")
         if do_backprop and current_epoch > self.epoch_for_weighting:
             spl = SymmetricSelfPacedLearning(current_epoch, gradients_map)
             weighted_loss = spl(result_fl, keys)
@@ -105,12 +105,12 @@ class FocalLossNonBatch_SPL(nn.Module):
             return result_fl.mean()
 
 
-class nnUNetTrainerV2_SoftDiceLoss_SPL_HardFirst(nnUNetTrainerV2):
+class nnUNetTrainerV2_SoftDiceLoss_SPL_HardAlways(nnUNetTrainerV2):
     def __init__(self, plans_file, fold, output_folder=None, dataset_directory=None, batch_dice=True, stage=None,
                  unpack_data=True, deterministic=True, fp16=False):
         super().__init__(plans_file, fold, output_folder, dataset_directory, batch_dice, stage,
                                               unpack_data, deterministic, fp16)
-        print("Setting up self.loss = SoftDiceLoss_SPL_HardFirst")
+        print("Setting up self.loss = SoftDiceLoss_SPL_HardAlways")
         self.loss = SoftDiceLoss_SPL({'batch_dice': False, 'smooth': 1e-5, 'do_bg': False})
         self.save_latest_only = False
 
@@ -133,12 +133,12 @@ class nnUNetTrainerV2_SoftDiceLoss_SPL_Test(nnUNetTrainerV2):
         self.save_latest_only = False
 
 
-class nnUNetTrainerV2_FocalLossNonBatch_SPL_HardFirst(nnUNetTrainerV2):
+class nnUNetTrainerV2_FocalLossNonBatch_SPL_HardAlways(nnUNetTrainerV2):
     def __init__(self, plans_file, fold, output_folder=None, dataset_directory=None, batch_dice=True, stage=None,
                  unpack_data=True, deterministic=True, fp16=False):
         super().__init__(plans_file, fold, output_folder, dataset_directory, batch_dice, stage,
                                               unpack_data, deterministic, fp16)
-        print("Setting up self.loss = FocalLossNonBatch_SPL_HardFirst")
+        print("Setting up self.loss = FocalLossNonBatch_SPL_HardAlways")
         self.loss = FocalLossNonBatch_SPL({}, {'batch_dice': False, 'smooth': 1e-5, 'do_bg': False})
         self.save_latest_only = False
 
@@ -160,42 +160,25 @@ class nnUNetTrainerV2_CELossNonBatch_SPL_Baseline(nnUNetTrainerV2):
         self.loss = FocalLossNonBatch_SPL({'gamma':0}, {'batch_dice': False, 'smooth': 1e-5, 'do_bg': False}, epoch_for_weighting=1000)
         self.save_latest_only = False
 
-class nnUNetTrainerV2_CELossNonBatch_SPL_HardFirst(nnUNetTrainerV2):
+class nnUNetTrainerV2_CELossNonBatch_SPL_HardAlways(nnUNetTrainerV2):
     def __init__(self, plans_file, fold, output_folder=None, dataset_directory=None, batch_dice=True, stage=None,
                  unpack_data=True, deterministic=True, fp16=False):
         super().__init__(plans_file, fold, output_folder, dataset_directory, batch_dice, stage,
                                               unpack_data, deterministic, fp16)
-        print("Setting up self.loss = CELossNonBatch_SPL")
+        print("Setting up self.loss = CELossNonBatch_SPL_HardAlways")
         self.loss = FocalLossNonBatch_SPL({'gamma':0}, {'batch_dice': False, 'smooth': 1e-5, 'do_bg': False})
         self.save_latest_only = False
 
 
 if __name__ == "__main__":
-    # loss = torch.tensor([0, 0.8, 0.9, 0.1, 0.5, 1])  # loss = 1 - Dice
-    # example_difficulty = loss
-    # print(f"difficulty={example_difficulty}")
-    # current_epoch = 100
-    # spl = SymmetricSelfPacedLearning(current_epoch)
-    # weight_matrix = spl.compute_weight_matrix(example_difficulty)
-    # print(f"epoch={current_epoch}, weight_matrix={weight_matrix}")
-    #
-    # current_epoch = 500
-    # spl = SymmetricSelfPacedLearning(current_epoch)
-    # weight_matrix = spl.compute_weight_matrix(example_difficulty)
-    # print(f"epoch={current_epoch}, weight_matrix={weight_matrix}")
-    #
-    # current_epoch = 800
-    # spl = SymmetricSelfPacedLearning(current_epoch)
-    # weight_matrix = spl.compute_weight_matrix(example_difficulty)
-    # print(f"epoch={current_epoch}, weight_matrix={weight_matrix}")
 
     size = (2, 2, 16, 320, 320)
     size_label = (2, 1, 16, 320, 320)
     pre = torch.softmax(torch.rand(size), dim=1)
     label = torch.randint(0, 2, size_label)
 
-    epoch = 900
-    dice_loss_spl = SoftDiceLoss_SPL({'batch_dice': False, 'smooth': 1e-5, 'do_bg': False})
-    # focal_loss_spl = FocalLossNonBatch_SPL({}, {'batch_dice': False, 'smooth': 1e-5, 'do_bg': False})
-    tmp = dice_loss_spl(pre, label, epoch, True, ['123', '789'], {'123':2, '345':100, '789':39})
+    epoch = 499
+    # dice_loss_spl = SoftDiceLoss_SPL({'batch_dice': False, 'smooth': 1e-5, 'do_bg': False})
+    focal_loss_spl = FocalLossNonBatch_SPL({}, {'batch_dice': False, 'smooth': 1e-5, 'do_bg': False})
+    tmp = focal_loss_spl(pre, label, epoch, True, ['123', '345'], {'123':2, '345':100, '789':39})
     print(tmp)
