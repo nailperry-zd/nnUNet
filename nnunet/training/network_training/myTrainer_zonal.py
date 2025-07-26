d492cf85569325190ac30c04093ef1dafc234a4a
Zoe Dan Zhang <dzha937@aucklanduni.ac.nz>
2025-07-26 19:21:59 +1200

    fix integrating SPL on ZSSMNet

--- nnunet/training/network_training/myTrainer_zonal.py
+++ nnunet/training/network_training/myTrainer_zonal.py
@@ -274,7 +274,7 @@ class myTrainer_zonal(nnUNetTrainer):
         self.network.do_ds = ds
         return ret
 
-    def run_iteration(self, data_generator, do_backprop=True, run_online_evaluation=False):
+    def run_iteration(self, data_generator, current_epoch, do_backprop=True, run_online_evaluation=False):
         """
         gradient clipping improves training stability
 
@@ -286,6 +286,7 @@ class myTrainer_zonal(nnUNetTrainer):
         data_dict = next(data_generator)
         data = data_dict['data']
         target = data_dict['target']
+        keys = data_dict['keys']
 
         nnunet.utilities.visualize_data_seg.visualize_data_seg_aug(data, target)
 
@@ -301,8 +302,8 @@ class myTrainer_zonal(nnUNetTrainer):
         if self.fp16:
             with autocast():
                 output = self.network(data)
-                del data
-                l = self.loss(output, target)
+                # del data
+                l = self.loss(output, target, current_epoch, do_backprop, keys, self.gradients_map)
 
             if do_backprop:
                 self.amp_grad_scaler.scale(l).backward()
@@ -310,6 +311,18 @@ class myTrainer_zonal(nnUNetTrainer):
                 torch.nn.utils.clip_grad_norm_(self.network.parameters(), 12)
                 self.amp_grad_scaler.step(self.optimizer)
                 self.amp_grad_scaler.update()
+
+                # Calculate the L2 norm for each batch
+                data_grad = data.grad.cpu().numpy()
+                for i in range(data_grad.shape[0]):  # Iterate over each batch
+                    norm = np.linalg.norm(data_grad[i])  # Calculate L2 norm
+                    norm_channels = []
+                    for j in range(data_grad.shape[1]):
+                        norm_per_channel = np.linalg.norm(data_grad[i][j])  # Calculate L2 norm for each channel
+                        norm_channels.append(norm_per_channel)
+                    self.gradients_map[keys[i]] = norm
+                    self.gradients_map_channels[keys[i]] = norm_channels
+                del data
         else:
             output = self.network(data)
             del data
