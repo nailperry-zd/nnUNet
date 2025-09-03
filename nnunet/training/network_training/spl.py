c71c685cf21ef97d9592594a700581610621ef96
Zoe Dan Zhang <dzha937@aucklanduni.ac.nz>
2025-09-03 15:10:26 +1200

    add: CE+FL for nnUNet_302

--- nnunet/training/network_training/spl.py
+++ nnunet/training/network_training/spl.py
@@ -192,7 +192,7 @@ class nnUNetTrainerV2_CELossNonBatch_SPL_HardFirst(nnUNetTrainerV2):
         self.loss = FocalLossNonBatch_SPL({'gamma':0}, {'batch_dice': False, 'smooth': 1e-5, 'do_bg': False})
         self.save_latest_only = False
 
-class nnUNet_Zonal_FLCELossNonBatch_SPL_HardFirst(nnUNetTrainerV2):
+class nnUNet_302_FLCELossNonBatch_SPL_HardFirst(nnUNetTrainerV2):
     def __init__(self, plans_file, fold, output_folder=None, dataset_directory=None, batch_dice=True, stage=None,
                  unpack_data=True, deterministic=True, fp16=False):
         super().__init__(plans_file, fold, output_folder, dataset_directory, batch_dice, stage,
@@ -202,7 +202,7 @@ class nnUNet_Zonal_FLCELossNonBatch_SPL_HardFirst(nnUNetTrainerV2):
         self.loss = FLCENonBatch_SPL({'gamma':2}, {'gamma':0}, max_num_epochs=self.max_num_epochs, max_weight=2)
         self.save_latest_only = False
 
-class nnUNet_Zonal_FLCELossNonBatch_SPL_HardFirst_MW4(nnUNetTrainerV2):
+class nnUNet_302_FLCELossNonBatch_SPL_HardFirst_MW4(nnUNetTrainerV2):
     def __init__(self, plans_file, fold, output_folder=None, dataset_directory=None, batch_dice=True, stage=None,
                  unpack_data=True, deterministic=True, fp16=False):
         super().__init__(plans_file, fold, output_folder, dataset_directory, batch_dice, stage,
