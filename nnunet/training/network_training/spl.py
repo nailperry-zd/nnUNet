0870bf74293a610ad27972d1613d0630ca565fad
Zoe Dan Zhang <dzha937@aucklanduni.ac.nz>
2025-10-05 23:41:33 +1300

    fix:change the trainer name

--- nnunet/training/network_training/spl.py
+++ nnunet/training/network_training/spl.py
@@ -180,13 +180,13 @@ class nnUNet_302_FLCELossNonBatch_SPL_HardFirst(nnUNetTrainerV2):
         self.loss = FLCENonBatch_SPL({'gamma':2}, {'gamma':0}, max_num_epochs=self.max_num_epochs, max_weight=2, reverse=True)
         self.save_latest_only = False
 
-class nnUNet_302_FLCELossNonBatch_SPL_HardFirst_MW4(nnUNetTrainerV2):
+class nnUNet_302_FLCELossNonBatch_SPL_LossHardFirst_MW4_500(nnUNetTrainerV2):
     def __init__(self, plans_file, fold, output_folder=None, dataset_directory=None, batch_dice=True, stage=None,
                  unpack_data=True, deterministic=True, fp16=False):
         super().__init__(plans_file, fold, output_folder, dataset_directory, batch_dice, stage,
                                               unpack_data, deterministic, fp16)
-        self.max_num_epochs = 1000
-        print("Setting up self.loss = FLCELossNonBatch_RSSPL_MW4")
+        self.max_num_epochs = 500
+        print("Setting up self.loss = FLCELossNonBatch_LossRSSPL_MW4")
         self.loss = FLCENonBatch_SPL({'gamma':2}, {'gamma':0}, max_num_epochs=self.max_num_epochs, max_weight=4, reverse=True)
         self.save_latest_only = False
 
