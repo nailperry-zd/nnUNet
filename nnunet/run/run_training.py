5b70bcc05282331f6d5f8188b2c76b22b3bf58f8
Zoe Dan Zhang <dzha937@aucklanduni.ac.nz>
2026-01-06 15:22:41 +1300

    fix: load weights for TZ-teacher

--- nnunet/run/run_training.py
+++ nnunet/run/run_training.py
@@ -174,7 +174,9 @@ def main():
                 trainer.load_latest_checkpoint()
             elif (not args.continue_training) and (args.pretrained_weights is not None):
                 # we start a new training. If pretrained_weights are set, use them
-                load_pretrained_weights(trainer.network, args.pretrained_weights)
+                # load_pretrained_weights(trainer.network, args.pretrained_weights)
+                load_pretrained_weights(trainer.net_teacher, args.pretrained_weights)
+                trainer.net_teacher.eval()
             else:
                 # new training without pretraine weights, do nothing
                 pass
