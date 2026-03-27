1f77fe725a7467c5bae0e16cb1a799122861dc53
Zoe Dan Zhang <dzha937@aucklanduni.ac.nz>
2026-03-28 12:40:10 +1300

    add: nnUNetTrainerV2_FLCELossNonBatch_EW500

--- nnunet/training/network_training/spl.py
+++ nnunet/training/network_training/spl.py
@@ -135,6 +135,16 @@ class nnUNetTrainerV2_FLCELossNonBatch_SPL_HardFirst(nnUNetTrainerV2):
         self.loss = FLCENonBatch_SPL({'gamma':2}, {'gamma':0})
         self.save_latest_only = False
 
+class nnUNetTrainerV2_FLCELossNonBatch_EW500(nnUNetTrainerV2):
+    def __init__(self, plans_file, fold, output_folder=None, dataset_directory=None, batch_dice=True, stage=None,
+                 unpack_data=True, deterministic=True, fp16=False):
+        super().__init__(plans_file, fold, output_folder, dataset_directory, batch_dice, stage,
+                                              unpack_data, deterministic, fp16)
+        print("Setting up self.loss = FLCELossNonBatch_EW")
+        self.max_num_epochs = 500
+        self.loss = FLCENonBatch_SPL({'gamma': 2}, {'gamma': 0}, epoch_for_weighting=self.max_num_epochs,
+                                     max_num_epochs=self.max_num_epochs)
+        self.save_latest_only = False
 
 if __name__ == "__main__":
     # loss = torch.tensor([0, 0.8, 0.9, 0.1, 0.5, 1])  # loss = 1 - Dice
